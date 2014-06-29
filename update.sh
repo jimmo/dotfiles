a17513a775ec6cd2ebceafe02fc70421d5f51967
Jim Mussared <jim.mussared@gmail.com>
2014-06-29 22:18:09 +1000

    first version of update.sh and x240 i3 config overrides

--- update.sh
+++ update.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-cat home/.i3/config > $HOME/.i3/config
-cat x240/home/.i3/config >> $HOME/.i3/config
+( (cd common && find . -type f) && ([ -d "$1" ] && cd $1 && find . -type f) ) | sort | uniq | while read file; do
+  base=`echo $file | cut -d/ -f2`
+  file=`echo $file | cut -d/ -f3-`
+  prefix=""
+  dest=""
+  if [ $base = "etc" ]; then
+    prefix="sudo "
+    dest="/etc"
+  fi
+  if [ $base = "home" ]; then
+    dest=$HOME
+  fi
+  $prefix mkdir -p `dirname $dest/$file`
+  [ -e $dest/$file ] && $prefix bash -c "echo > $dest/$file"
+  for part in common $1; do
+    [ -e $part/$base/$file ] && $prefix bash -c "cat $part/$base/$file >> $dest/$file"
+  done
+done
