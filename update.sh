0b94aa302f741ecb6647720feaf91782477eb35a
Jim Mussared <jim.mussared@gmail.com>
2019-02-02 20:46:06 +1100

    Set sublime as git editor

--- update.sh
+++ update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+DIFF_ONLY=0
+
+if [ "$1" = "-d" ]; then
+    DIFF_ONLY=1
+    echo "Diff only..."
+fi
+
 # Get all the files that we're going to write.
 # This is the union of all the paths in both 'common' and the specified host dirs.
 for dir in common $*; do
     ([ -d "$dir" ] && find $dir -type f)
 done | sort | uniq | grep -v '[.]prepend$' | while read file; do
     # 'base' is the top-level dir (e.g. 'home', 'etc').
-    base=`echo $file | cut -d/ -f2`
+    base=`echo "$file" | cut -d/ -f2`
     # 'file' is the path (e.g. '.i3/config', 'X11/xorg.conf.d/20-foo.conf').
-    file=`echo $file | cut -d/ -f3-`
+    file=`echo "$file" | cut -d/ -f3-`
 
     # Optional prefix for running commands (e.g. 'sudo', or 'echo' for debugging).
     prefix=""
@@ -16,19 +23,19 @@ done | sort | uniq | grep -v '[.]prepend$' | while read file; do
 
     # Things going in /etc need sudo.
     if [ $base = "etc" ]; then
-	prefix="sudo "
-	dest="/etc"
+        prefix="sudo "
+        dest="/etc"
     fi
 
     # 'home' is $HOME
     if [ $base = "home" ]; then
-	dest=$HOME
+        dest=$HOME
     fi
 
     # Don't allow directories other than /etc and $HOME.
     if [ -z "$dest" ]; then
-	echo "Not sure where to write $base"
-	exit 1
+       echo "Not sure where to write $base"
+       exit 1
     fi
 
     # Enable for debugging.
@@ -39,33 +46,41 @@ done | sort | uniq | grep -v '[.]prepend$' | while read file; do
 
     # Prepend any '.prepend' files.
     for part in common $*; do
-	[ -e $part/$base/$file.prepend ] && cat $part/$base/$file.prepend >> /tmp/dotfile
+       [ -e "$part/$base/$file.prepend" ] && cat "$part/$base/$file.prepend" >> /tmp/dotfile
     done
 
     # Then the main content.
     for part in common $*; do
-	[ -e $part/$base/$file ] && cat $part/$base/$file >> /tmp/dotfile
+       [ -e "$part/$base/$file" ] && cat "$part/$base/$file" >> /tmp/dotfile
     done
 
     # Append any '.append' files.
     for part in common $*; do
-	[ -e $part/$base/$file.append ] && cat $part/$base/$file.append >> /tmp/dotfile
+       [ -e "$part/$base/$file.append" ] && cat "$part/$base/$file.append" >> /tmp/dotfile
     done
 
-    if diff /tmp/dotfile $dest/$file > /dev/null; then
-      echo "$dest/$file [skip]"
+    if diff "/tmp/dotfile" "$dest/$file" > /dev/null; then
+        echo "$dest/$file [skip]"
     else
-      echo "$dest/$file [update]"
-      $prefix mkdir -p `dirname $dest/$file`
-      $prefix bash -c "cat /tmp/dotfile > $dest/$file"
+        if [ $DIFF_ONLY -eq 1 ]; then
+            diff "/tmp/dotfile" "$dest/$file"
+        else
+            echo "$dest/$file [update]"
+            $prefix mkdir -p `dirname $dest/$file`
+            $prefix bash -c "cat /tmp/dotfile > '$dest/$file'"
+        fi
     fi
 
-    # Anything in bin/ gets +x.
-    if [[ $file =~ ^bin/ ]]; then
-	chmod +x $dest/$file
+    if [ $DIFF_ONLY -eq 0 ]; then
+        # Anything in bin/ gets +x.
+        if [[ "$file" =~ ^bin/ ]]; then
+            chmod +x "$dest/$file"
+        fi
     fi
 done
 
-# Compile .emacs --> .emacs.elc
-update_script=$PWD/update-emacs.el
-(cd ~; emacs -nw -q --script $update_script)
+if [ $DIFF_ONLY -eq 0 ]; then
+    # Compile .emacs --> .emacs.elc
+    update_script=$PWD/update-emacs.el
+    (cd ~; emacs -nw -q --script $update_script)
+fi
