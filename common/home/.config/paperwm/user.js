388d74e80f9147fae3dd0c45e0ddb8aca7a5415e
Jim Mussared <jim.mussared@gmail.com>
2021-01-19 15:20:14 +1100

    paperwm config

---
+++ common/home/.config/paperwm/user.js
@@ -0,0 +1,46 @@
+// -*- mode: gnome-shell -*-
+
+var Meta = imports.gi.Meta;
+var Clutter = imports.gi.Clutter;
+var St = imports.gi.St;
+var Main = imports.ui.main;
+var Shell = imports.gi.Shell;
+
+// Extension local imports
+var Extension, Me, Tiling, Utils, App, Keybindings, Examples;
+
+function init() {
+    // Runs _only_ once on startup
+
+    // Initialize extension imports here to make gnome-shell-reload work
+    Extension = imports.misc.extensionUtils.getCurrentExtension();
+    Me = Extension.imports.user;
+    Tiling = Extension.imports.tiling;
+    Utils = Extension.imports.utils;
+    Keybindings = Extension.imports.keybindings;
+    Examples = Extension.imports.examples;
+    App = Extension.imports.app;
+}
+
+function enable() {
+    // Runs on extension reloads, eg. when unlocking the session
+
+    Tiling.defwinprop({
+        wm_class: "gnome-control-center",
+        scratch_layer: true,
+    });
+
+    Tiling.defwinprop({
+        wm_class: "org.gnome.Shell.Extensions",
+        scratch_layer: true,
+    });
+
+    Tiling.defwinprop({
+        wm_class: "gnome-tweaks",
+        scratch_layer: true,
+    });
+}
+
+function disable() {
+    // Runs on extension reloads eg. when locking the session (`<super>L).
+}
