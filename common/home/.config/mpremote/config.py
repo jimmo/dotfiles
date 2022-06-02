74f696b93582ad21cd4ab1cc556a6542f1b46326
Jim Mussared <jim.mussared@gmail.com>
2022-06-02 10:47:20 +1000

    Update

--- common/home/.config/mpremote/config.py
+++ common/home/.config/mpremote/config.py
@@ -1,4 +1,12 @@
 commands = {
-    "sf6": "connect id:334E33683138",
+    "sf6_green": "connect id:334E33683138",
+    "sf6_red": "connect id:336A33713138",
+    "wb55_red": "connect id:205F3781594D",
+    "wb55_green": "connect id:20633793594D",
     "bl": "bootloader",
+    "littlefs": [
+            "exec",
+            "--no-follow",
+            "import os, pyb, machine; os.umount('/flash'); os.VfsLfs2.mkfs(pyb.Flash(start=0)); os.mount(pyb.Flash(start=0), '/flash'); os.chdir('/flash'); machine.reset()",
+    ],
 }
